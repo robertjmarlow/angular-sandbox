ebe5fa734f74330e4405deb6dddee525cecc93d9
robertjmarlow <robertjmarlow@gmail.com>
2026-04-17 14:28:40 -0500

    Adding a required input for housingLocation

--- first-app/src/app/housing-location/housing-location.ts
+++ first-app/src/app/housing-location/housing-location.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {HousingLocationInfo} from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -11,5 +12,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./housing-location.css'],
 })
 export class HousingLocation {
-
+  housingLocation = input.required<HousingLocationInfo>();
 }

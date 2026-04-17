d3837a432508f824c35a1dd155449c05d65338f5
robertjmarlow <robertjmarlow@gmail.com>
2026-04-17 13:57:42 -0500

    Adding housinglocation

--- first-app/src/app/home/home.ts
+++ first-app/src/app/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HousingLocation} from '../housing-location/housing-location';
+import {HousingLocationInfo} from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -18,5 +19,16 @@ import {HousingLocation} from '../housing-location/housing-location';
   styleUrls: ['./home.css'],
 })
 export class Home {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
 
+  housingLocation: HousingLocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }

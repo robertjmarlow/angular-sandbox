a0152c0e204f02e43fdccceb240085beff2899ff
robertjmarlow <robertjmarlow@gmail.com>
2026-04-22 15:12:17 -0500

    Using an actual HTTP request

--- first-app/src/app/details/details.ts
+++ first-app/src/app/details/details.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {ChangeDetectorRef, Component, inject} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {HousingService} from '../housing.service';
 import {HousingLocationInfo} from '../housinglocation';
@@ -46,6 +46,7 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
   styleUrls: ['./details.css'],
 })
 export class Details {
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocationInfo | undefined;
@@ -57,7 +58,10 @@ export class Details {
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    this.housingService.getHousingLocationById(housingLocationId).then((housingLocation) => {
+      this.housingLocation = housingLocation;
+      this.changeDetectorRef.markForCheck();
+    });
   }
 
     submitApplication() {

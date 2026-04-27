e5ac759dc0630e7cd572daea5ff5e1a6613ab1ef
robertjmarlow <robertjmarlow@gmail.com>
2026-04-27 13:02:06 -0500

    Getting a super simple test running

---
+++ first-app/test/details/details.spec.ts
@@ -0,0 +1,13 @@
+import { assert, expect, test } from 'vitest'
+import {TestBed} from '@angular/core/testing';
+import {Details} from 'src/app/details/details';
+
+// test('Details', () => {
+
+// })
+
+test('Math.sqrt()', () => {
+  expect(Math.sqrt(4)).toBe(2)
+  expect(Math.sqrt(144)).toBe(12)
+  expect(Math.sqrt(2)).toBe(Math.SQRT2)
+})
